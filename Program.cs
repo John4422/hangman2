17af44777500b46fe20ee3600e9f8d2cb041f92d
John4422 <Johnstein456@hotmail.com>
2022-12-13 11:34:16 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace hangman2
+{
+    internal class Program
+    {
+        int fail = 0;
+        public static void Drawhang(int stage)
+        {
+            if (stage == 0)
+            {
+                Console.WriteLine("+---+");
+                Console.WriteLine("|   |");
+                Console.WriteLine("    |");
+                Console.WriteLine("    |");
+                Console.WriteLine("    |");
+                Console.WriteLine("    |");
+              Console.WriteLine("=========");
+            }
+            if (stage == 1)
+            {
+                "+---+"
+                "|   |"
+                "O   |"
+                "    |"
+                "    |"
+                "    |"
+              =========
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
